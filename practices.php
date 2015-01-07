4d2a268f2890d73959a23e51057fa8b0fc8032ea
Mikael Nortikyan <nortikyan99@gmail.com>
2015-01-07 12:28:23 -0800

    Lecture 1
    Finished Lecture 1

---
+++ practices.php
@@ -0,0 +1,88 @@
+<?php 
+//Example 1//
+
+	class Cars{
+		//body
+		//body
+		//body
+	}
+
+	class Clothes{
+		//body
+		//body
+		//body
+	}
+
+	class Speakers{
+		//body
+		//body
+		//body
+	}
+
+//Example 2
+
+	$phone1 = newPhone();
+	$phone2 = newPhone();
+
+	$tablet1 =  newTablet()
+	$tablet2 = newTablet()
+
+	$cola1 = newCola();
+	$cola2 = newCola();
+
+//Example 3
+
+	class Phone{
+		public $brand = "LG";
+		public $color = "Black";
+		public $type = "LG G2";
+		public $OS = "Android";
+	}
+
+	class Car{
+		public $brand = "Mercedes - Benz";
+		public $color = "White";
+		public $Engine = "V12";
+		public $Maker = "Germany";
+	}
+
+	class Shirt{
+		public $brand = "Polo";
+		public $color = "Black";
+		public $Material = "Cotton";
+		public $Type = "Collar";
+	}
+
+// Example 4
+
+		$phone1 = new Phone();
+		print $phone>brand;
+		// Default brand
+
+		$tablet = new Tablet();
+		print $tablet>model;
+		// Default model
+
+		$speaker = new Speaker();
+		print $speaker>color;
+		// Default color
+
+//Example 5
+
+		public function Method( $argument1, $argument2) {
+		// body
+		// body
+		// body
+	}
+
+	public function Method2( $argument1, $argument2) {
+		// body
+		// body
+		// body
+	}
+
+	public function Method3( $argument1, $argument2) {
+		// body
+		// body
+		// body
+	}
